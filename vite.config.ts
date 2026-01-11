3507ba322bae0733e65a4a6452808180361a3204
Marcos Gregorios <gregoriodata@gmail.com>
2026-01-11 18:27:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/meu-portfolio-dados/",
   plugins: [react()],
+  base: "/meu-portfolio-dados/",
 })
